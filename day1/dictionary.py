8d5b05d6395e95bd830c8461a1a6cb355ea95897
staryjie <staryjie@163.com>
2018-03-07 12:50:31 +0800

    字典介绍及字典的特性

---
+++ day1/dictionary.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+##字典
+'''
+一种key:value的数据类型
+key必须可hash且在当前字典中唯一
+vaule可以存放任意多个值，可以修改，可以不唯一
+无序
+查找速度快
+'''
+
+info = {
+    "user1":{
+        "password":"123456",
+        "age":20,
+        "address":"浙江省杭州市西湖区",
+        "phone":18989474624,
+    },
+    "user2":{
+        "password":"223456",
+        "age":21,
+        "address":"浙江省杭州市上城区",
+        "phone":18982341624,
+    },
+}
