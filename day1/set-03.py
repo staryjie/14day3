da6e30df37cbb29eac00dbac6739dfcd1240194d
staryjie <staryjie@163.com>
2018-03-07 16:45:32 +0800

    集合的其他功能

--- day1/set-03.py
+++ day1/set-03.py
@@ -5,8 +5,8 @@ s1 = {1,2,3,4,5}
 s2 = {2,3,4,7,8,9}
 
 print(s1.difference(s2))
-#### difference_update(self, *args, **kwargs)
-#### 从当前集合中删除和B中相同的元素，更新自己
+# difference_update(self, *args, **kwargs)
+# 从当前集合中删除和B中相同的元素，更新自己
 print(s1.difference_update(s2))
 
 # intersection(self, *args, **kwargs)
